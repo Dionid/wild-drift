c518eb559b0db18d14c61ec867388d7c141a1ee8
David Shek <ditreyw@gmail.com>
2024-09-12 16:43:58 +0800

    feat: more accurate alpha

--- src/cengine/rendering.h
+++ src/cengine/rendering.h
@@ -18,7 +18,6 @@ namespace cen {
 
 class CanvasItem2D {
     public:
-        Vector2 previousPosition;
         node_id_t id;
         int zOrder;
         Vector2 position;
@@ -26,19 +25,17 @@ class CanvasItem2D {
 
         CanvasItem2D(
             Vector2 position,
-            Vector2 previousPosition,
             float alpha,
             int zOrder,
             uint16_t id = 0
         ) {
             this->position = position;
-            this->previousPosition = previousPosition;
             this->zOrder = zOrder;
             this->alpha = alpha;
             this->id = id;
         }
 
-        virtual void Render(float alpha) = 0;
+        virtual void Render() = 0;
 };
 
 class LineCanvasItem2D: public CanvasItem2D {
@@ -49,30 +46,22 @@ class LineCanvasItem2D: public CanvasItem2D {
 
         LineCanvasItem2D(
             Vector2 position,
-            Vector2 previousPosition,
             float length,
             Color color = WHITE,
             float alpha = 1.0f,
             int zOrder = 0,
             uint16_t id = 0
-        ): CanvasItem2D(position, previousPosition, alpha, zOrder, id) {
+        ): CanvasItem2D(position, alpha, zOrder, id) {
             this->length = length;
             this->alpha = alpha;
             this->color = color;
         }
 
-        void Render(float alpha) override {
-            Vector2 newPosition = Vector2Lerp(
-                this->previousPosition,
-                this->position,
-                alpha
-            );
-
-            Vector2 end = {
-                newPosition.x,
-                newPosition.y + this->length
+        void Render() override {            Vector2 end = {
+                this->position.x,
+                this->position.y + this->length
             };
-            DrawLineV(newPosition, end, ColorAlpha(this->color, this->alpha));
+            DrawLineV(this->position, end, ColorAlpha(this->color, this->alpha));
         }
 };
 
@@ -84,33 +73,26 @@ class CircleCanvasItem2D: public CanvasItem2D {
 
         CircleCanvasItem2D(
             Vector2 position,
-            Vector2 previousPosition,
             float radius,
             Color color = WHITE,
             float alpha = 1.0f,
             bool fill = true,
             int zOrder = 0,
             uint16_t id = 0
-        ): CanvasItem2D(position, previousPosition, alpha, zOrder, id) {
+        ): CanvasItem2D(position, alpha, zOrder, id) {
             this->radius = radius;
             this->alpha = alpha;
             this->color = color;
             this->fill = fill;
         }
 
-        void Render(float alpha) override {
-            Vector2 newPosition = Vector2Lerp(
-                this->previousPosition,
-                this->position,
-                alpha
-            );
-
+        void Render() override {
             if (this->fill) {
-                DrawCircleV(newPosition, this->radius, ColorAlpha(this->color, this->alpha));
+                DrawCircleV(this->position, this->radius, ColorAlpha(this->color, this->alpha));
                 return;
             }
 
-            DrawCircleLinesV(newPosition, this->radius, ColorAlpha(this->color, this->alpha));
+            DrawCircleLinesV(this->position, this->radius, ColorAlpha(this->color, this->alpha));
         }
 };
 
@@ -121,25 +103,18 @@ class RectangleCanvasItem2D: public CanvasItem2D {
 
         RectangleCanvasItem2D(
             Vector2 position,
-            Vector2 previousPosition,
             cen::Size size,
             Color color = WHITE,
             float alpha = 1.0f,
             int zOrder = 0,
             uint16_t id = 0
-        ): CanvasItem2D(position, previousPosition, alpha, zOrder, id) {
+        ): CanvasItem2D(position, alpha, zOrder, id) {
             this->size = size;
             this->color = color;
         }
 
-        void Render(float alpha) override {
-            Vector2 newPosition = Vector2Lerp(
-                this->previousPosition,
-                this->position,
-                alpha
-            );
-
-            DrawRectangle(newPosition.x - this->size.width * 0.5, newPosition.y - this->size.height * 0.5, this->size.width, this->size.height, ColorAlpha(this->color, this->alpha));
+        void Render() override {
+            DrawRectangle(this->position.x - this->size.width * 0.5, this->position.y - this->size.height * 0.5, this->size.width, this->size.height, ColorAlpha(this->color, this->alpha));
         }
 };
 
@@ -153,7 +128,6 @@ class ButtonCanvasItem2D: public CanvasItem2D {
 
         ButtonCanvasItem2D(
             Vector2 position,
-            Vector2 previousPosition,
             cen::BtnState state,
             const char* btnText,
             int btnTextFontSize,
@@ -162,7 +136,7 @@ class ButtonCanvasItem2D: public CanvasItem2D {
             float alpha = 1.0f,
             int zOrder = 0,
             node_id_t id = 0
-        ): CanvasItem2D(position, previousPosition, alpha, zOrder, id) {
+        ): CanvasItem2D(position, alpha, zOrder, id) {
             this->state = state;
             this->text = btnText;
             this->fontSize = btnTextFontSize;
@@ -184,8 +158,7 @@ class ButtonCanvasItem2D: public CanvasItem2D {
             };
         }
 
-        void Render(float alpha) override {
-            switch (state) {
+        void Render() override {            switch (state) {
                 case BtnState::Normal:
                     DrawRectangleRec(
                         btnRect,
@@ -206,16 +179,11 @@ class ButtonCanvasItem2D: public CanvasItem2D {
                     break;
             }
 
-            Vector2 newPosition = Vector2Lerp(
-                this->previousPosition,
-                this->position,
-                alpha
-            );
 
             DrawText(
                 this->text,
-                newPosition.x - MeasureText(this->text, this->fontSize) * this->anchor.x,
-                newPosition.y - this->fontSize * this->anchor.y,
+                this->position.x - MeasureText(this->text, this->fontSize) * this->anchor.x,
+                this->position.y - this->fontSize * this->anchor.y,
                 this->fontSize,
                 BLACK
             );
@@ -230,29 +198,22 @@ class TextCanvasItem2D: public CanvasItem2D {
 
         TextCanvasItem2D(
             Vector2 position,
-            Vector2 previousPosition,
             std::string text,
             int fontSize,
             Color color,
             float alpha = 1.0f,
             int zOrder = 0
-        ): CanvasItem2D(position, previousPosition, alpha, zOrder) {
+        ): CanvasItem2D(position, alpha, zOrder) {
             this->text = text;
             this->fontSize = fontSize;
             this->color = color;
         }
 
-        void Render(float alpha) override {
-            Vector2 newPosition = Vector2Lerp(
-                this->previousPosition,
-                this->position,
-                alpha
-            );
-
+        void Render() override {
             DrawText(
                 this->text.c_str(),
-                newPosition.x,
-                newPosition.y,
+                this->position.x,
+                this->position.y,
                 this->fontSize,
                 this->color
             );
@@ -268,23 +229,27 @@ class RenderingEngine2D {
         std::mutex secondBufferMutex;
 
     public:
-        std::atomic<float> firstBufferAlpha;
         render_buffer firstBuffer;
-        std::atomic<float> secondBufferAlpha;
         render_buffer secondBuffer;
 
         void MapNode2D(
             render_buffer& activeRenderBuffer,
-            cen::Node2D* node2D
+            cen::Node2D* node2D,
+            float alpha
         ) {
             Vector2 position = node2D->GlobalPosition();
             Vector2 previousPosition = node2D->PreviousGlobalPosition();
 
+            Vector2 newGlobalPosition = Vector2Lerp(
+                previousPosition,
+                position,
+                alpha
+            );
+
             if (auto lineView = dynamic_cast<cen::LineView*>(node2D)) {
                 activeRenderBuffer.push_back(
                     std::make_unique<LineCanvasItem2D>(
-                        position,
-                        previousPosition,
+                        newGlobalPosition,
                         lineView->length,
                         lineView->color,
                         lineView->alpha,
@@ -295,8 +260,7 @@ class RenderingEngine2D {
             } else if (auto circleView = dynamic_cast<cen::CircleView*>(node2D)) {
                 activeRenderBuffer.push_back(
                     std::make_unique<CircleCanvasItem2D>(
-                        position,
-                        previousPosition,
+                        newGlobalPosition,
                         circleView->radius,
                         circleView->color,
                         circleView->alpha,
@@ -308,8 +272,7 @@ class RenderingEngine2D {
             } else if (auto rectangleView = dynamic_cast<cen::RectangleView*>(node2D)) {
                 activeRenderBuffer.push_back(
                     std::make_unique<RectangleCanvasItem2D>(
-                        position,
-                        previousPosition,
+                        newGlobalPosition,
                         rectangleView->size,
                         rectangleView->color,
                         rectangleView->alpha,
@@ -320,8 +283,7 @@ class RenderingEngine2D {
             } else if (auto buttonView = dynamic_cast<cen::Btn*>(node2D)) {
                 activeRenderBuffer.push_back(
                     std::make_unique<ButtonCanvasItem2D>(
-                        position,
-                        previousPosition,
+                        newGlobalPosition,
                         buttonView->state,
                         buttonView->text,
                         buttonView->fontSize,
@@ -333,8 +295,7 @@ class RenderingEngine2D {
                 
                 activeRenderBuffer.push_back(
                     std::make_unique<TextCanvasItem2D>(
-                        position,
-                        previousPosition,
+                        newGlobalPosition,
                         std::string(textView->text).c_str(),
                         textView->fontSize,
                         textView->color
@@ -357,7 +318,8 @@ class RenderingEngine2D {
 
                 this->MapNode2D(
                     writeBuffer,
-                    node
+                    node,
+                    alpha
                 );
             }
 
@@ -369,12 +331,10 @@ class RenderingEngine2D {
                 if (activeRenderBufferInd.load(std::memory_order_acquire) == 0) {
                     // std::lock_guard<std::mutex> lock(secondBufferMutex);
                     secondBuffer = std::move(writeBuffer);
-                    secondBufferAlpha.store(alpha, std::memory_order_release);
                     activeRenderBufferInd.store(1, std::memory_order_release);
                 } else {
                     // std::lock_guard<std::mutex> lock(firstBufferMutex);
                     firstBuffer = std::move(writeBuffer);
-                    firstBufferAlpha.store(alpha, std::memory_order_release);
                     activeRenderBufferInd.store(0, std::memory_order_release);
                 }
             }
@@ -384,16 +344,12 @@ class RenderingEngine2D {
             if (activeRenderBufferInd.load(std::memory_order_acquire) == 0) {
                 // std::lock_guard<std::mutex> lock(firstBufferMutex);
                 for (const auto& item: firstBuffer) {
-                    item->Render(
-                        firstBufferAlpha.load(std::memory_order_acquire)
-                    );
+                    item->Render();
                 }
             } else {
                 // std::lock_guard<std::mutex> lock(secondBufferMutex);
                 for (const auto& item: secondBuffer) {
-                    item->Render(
-                        secondBufferAlpha.load(std::memory_order_acquire)
-                    );
+                    item->Render();
                 }
             }
         };
